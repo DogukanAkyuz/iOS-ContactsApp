cf3b9dacfa2b86e62cab7b94b305c9f9a391b14b
DogukanAkyuz <sdogukanakyuz@gmail.com>
2024-11-29 16:34:41 +0300

    Refactor project to follow MVVM architecture. Updated the structure of the app with ViewModel and Model layers. Still to implement database functionality.

--- ContactsApp/UI/View/Homepage.swift
+++ ContactsApp/UI/View/Homepage.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct Homepage: View {
-    @State private var contactList = [Contacts]()
     @State private var searchedWord = ""
     
+    @ObservedObject var viewModel = HomepageVM()
     
     func del(at offsets:IndexSet){
-        let contact = contactList[offsets.first!]
-        contactList.remove(at: offsets.first!)
+        let contact = viewModel.contactList[offsets.first!]
+        viewModel.contactList.remove(at: offsets.first!)
         print("Contact Deleted: \(contact.contact_id!)")
     }
     
@@ -23,7 +23,7 @@ struct Homepage: View {
     var body: some View {
         NavigationStack{
             List{
-                ForEach(contactList){ contact in
+                ForEach(viewModel.contactList){ contact in
                     NavigationLink(destination: ContactDetailView(contact: contact)) {
                         ContactRow(contact: contact)
                     }.listRowBackground(Color.mint)
@@ -45,19 +45,12 @@ struct Homepage: View {
                     }
                 }
                 .onAppear{
-                    var list = [Contacts]()
-                    let c1 = Contacts(contact_id: 1, contact_name: "Ahmet", contact_tel: "1111")
-                    let c2 = Contacts(contact_id: 2, contact_name: "Mehmet", contact_tel: "2222")
-                    let c3 = Contacts(contact_id: 3, contact_name: "Zeynep", contact_tel: "3333")
-                    list.append(c1)
-                    list.append(c2)
-                    list.append(c3)
-                    contactList = list
+                    viewModel.loadContacts()
                 }
                 .toolbarBackground(.mint, for: .navigationBar)
                 .searchable(text: $searchedWord, prompt: "Search")
                 .onChange(of: searchedWord){_ , new in
-                    print("Search: \(new)")
+                    viewModel.search(searchedWord: new)
                 }
         }
     }
--- ContactsApp/UI/ViewModel/HomepageVM.swift
+++ ContactsApp/UI/ViewModel/HomepageVM.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 class HomepageVM : ObservableObject {
+    @Published var contactList = [Contacts]()
+    
     func del(at offsets:IndexSet){
         let contact = contactList[offsets.first!]
         contactList.remove(at: offsets.first!)
         print("Contact Deleted: \(contact.contact_id!)")
+    }
+    func loadContacts(){
+        var list = [Contacts]()
+        let c1 = Contacts(contact_id: 1, contact_name: "Ahmet", contact_tel: "1111")
+        let c2 = Contacts(contact_id: 2, contact_name: "Mehmet", contact_tel: "2222")
+        let c3 = Contacts(contact_id: 3, contact_name: "Zeynep", contact_tel: "3333")
+        list.append(c1)
+        list.append(c2)
+        list.append(c3)
+        contactList = list
+    }
+    func search(searchedWord: String){
+        print("Search: \(searchedWord)" )
     }
 }
