7f0ce88bc313e159eb1a5620e2749955ec9bff0f
DogukanAkyuz <sdogukanakyuz@gmail.com>
2025-01-06 17:13:43 +0300

    Feat: Set up SQLite database connection and ensure table existence

--- ContactsApp/UI/View/Homepage.swift
+++ ContactsApp/UI/View/Homepage.swift
@@ -9,49 +9,63 @@ import SwiftUI
 
 struct Homepage: View {
     @State private var searchedWord = ""
-    
+
     @ObservedObject var viewModel = HomepageVM()
-    
-    func del(at offsets:IndexSet){
+
+    func del(at offsets: IndexSet) {
         let contact = viewModel.contactList[offsets.first!]
         viewModel.contactList.remove(at: offsets.first!)
         print("Contact Deleted: \(contact.contact_id!)")
     }
-    
-    
-    
+
     var body: some View {
-        NavigationStack{
-            List{
-                ForEach(viewModel.contactList){ contact in
+        NavigationStack {
+            List {
+                ForEach(viewModel.contactList) { contact in
                     NavigationLink(destination: ContactDetailView(contact: contact)) {
                         ContactRow(contact: contact)
                     }.listRowBackground(Color.mint)
-               
+
                 }.onDelete(perform: del)
-                
             }
             .background(.mint)
             .scrollContentBackground(.hidden)
             .navigationTitle("Contacts")
-            
 
-                .toolbar{
-                    ToolbarItem(placement: .topBarTrailing){
-                        NavigationLink(destination: ContactAddView()){
-                            Image(systemName: "plus").foregroundStyle(Color(UIColor.black))
-                        }
-                        
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(destination: ContactAddView()) {
+                        Image(systemName: "plus").foregroundStyle(Color(UIColor.black))
                     }
                 }
-                .onAppear{
-                    viewModel.loadContacts()
-                }
-                .toolbarBackground(.mint, for: .navigationBar)
-                .searchable(text: $searchedWord, prompt: "Search")
-                .onChange(of: searchedWord){_ , new in
-                    viewModel.search(searchedWord: new)
-                }
+            }
+            .onAppear {
+                copyDB()
+                viewModel.loadContacts()
+            }
+            .toolbarBackground(.mint, for: .navigationBar)
+            .searchable(text: $searchedWord, prompt: "Search")
+            .onChange(of: searchedWord) { _, new in
+                viewModel.search(searchedWord: new)
+            }
+        }
+    }
+
+    func copyDB() {
+        let bundle = Bundle.main.path(forResource: "rehber", ofType: ".sqlite")
+        let dbPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let destinationPath = URL(fileURLWithPath: dbPath).appendingPathComponent("rehber.sqlite")
+        let fm = FileManager.default
+
+        if fm.fileExists(atPath: destinationPath.path) {
+            print("DB already exists")
+        } else {
+            do {
+                try fm.copyItem(atPath: bundle!, toPath: destinationPath.path)
+                print("DB copied")
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
--- ContactsApp/UI/ViewModel/HomepageVM.swift
+++ ContactsApp/UI/ViewModel/HomepageVM.swift
@@ -10,20 +10,44 @@ import Foundation
 class HomepageVM : ObservableObject {
     @Published var contactList = [Contacts]()
     
+    let db: FMDatabase?
+    
+    init(){
+        let dbPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let destinationPath = URL(fileURLWithPath: dbPath).appendingPathComponent("rehber.sqlite")
+        db = FMDatabase(path: destinationPath.path)
+        }
+    
     func del(at offsets:IndexSet){
         let contact = contactList[offsets.first!]
         contactList.remove(at: offsets.first!)
         print("Contact Deleted: \(contact.contact_id!)")
     }
     func loadContacts(){
+        db?.open()
         var list = [Contacts]()
-        let c1 = Contacts(contact_id: 1, contact_name: "Ahmet", contact_tel: "1111")
-        let c2 = Contacts(contact_id: 2, contact_name: "Mehmet", contact_tel: "2222")
-        let c3 = Contacts(contact_id: 3, contact_name: "Zeynep", contact_tel: "3333")
-        list.append(c1)
-        list.append(c2)
-        list.append(c3)
-        contactList = list
+        
+        do {
+            let result = try db!.executeQuery("SELECT * FROM kisiler", values: nil)
+            while result.next(){
+                let contact_id = Int(result.string(forColumn: "kisi_id"))!
+                let contact_name = result.string(forColumn: "kisi_ad")!
+                let contact_tel = result.string(forColumn: "kisi_tel")!
+                
+                let contact = Contacts(contact_id: contact_id, contact_name: contact_name, contact_tel: contact_tel)
+                list.append(contact)
+              
+            }
+            
+            contactList = list
+            
+        }catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        
+        db?.close()
     }
     func search(searchedWord: String){
         print("Search: \(searchedWord)" )
