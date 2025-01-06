d488bcbb4d74a70b756cc5621494fea77bb07b44
DogukanAkyuz <sdogukanakyuz@gmail.com>
2025-01-06 17:29:25 +0300

    Feat: Contact add and update operations in the database
    - Resolved issue where added or updated contacts were not persisting after app restart.
    - Ensured database changes are correctly saved and retained across sessions.
    - Improved database management for consistent contact data handling.

--- ContactsApp/UI/ViewModel/ContactAddVM.swift
+++ ContactsApp/UI/ViewModel/ContactAddVM.swift
@@ -8,7 +8,25 @@
 import Foundation
 
 class ContactAddVM {
+    
+    let db: FMDatabase?
+    
+    init(){
+        let dbPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let destinationPath = URL(fileURLWithPath: dbPath).appendingPathComponent("rehber.sqlite")
+        db = FMDatabase(path: destinationPath.path)
+        }
+    
     func save(contact_name:String, contact_tel:String){
-        print("Contact Added: \(contact_name) - \(contact_tel)")
+        db?.open()
+        
+        do{
+            try db!.executeUpdate("INSERT INTO kisiler (kisi_ad, kisi_tel) VALUES (?,?)", values: [contact_name, contact_tel])
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        db?.close()
     }
+    
 }
--- ContactsApp/UI/ViewModel/ContactDetailVM.swift
+++ ContactsApp/UI/ViewModel/ContactDetailVM.swift
@@ -8,8 +8,25 @@
 import Foundation
 
 class ContactDetailVM {
+    
+    let db: FMDatabase?
+    
+    init(){
+        let dbPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let destinationPath = URL(fileURLWithPath: dbPath).appendingPathComponent("rehber.sqlite")
+        db = FMDatabase(path: destinationPath.path)
+        }
+    
     func update(contact_id:Int, contact_name:String, contact_tel:String){
-        print("Contact Updated: \(contact_id) - \(contact_name) - \(contact_tel)")
+        db?.open()
+        
+        do{
+            try db!.executeUpdate("UPDATE kisiler SET kisi_ad = ?, kisi_tel = ? WHERE kisi_id = ?", values: [contact_name, contact_tel, contact_id])
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        db?.close()
     }
     
 }
